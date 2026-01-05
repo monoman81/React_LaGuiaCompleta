c8687b94ed052555e5567f29a6eee2f4a2a1c90f
carlosp <carlosp@MX2-CARLOSP-OLD>
2026-01-04 19:03:04 -0600

    Productos PERN. Inicio de proyecto. Inicio de la REST API.

---
+++ productos-pern/rest-api/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express"
+
+const server = express()
+
+//ROUTING
+server.get('/', (req, res) => {
+    res.json('Desde GET')
+})
+
+server.post('/', (req, res) => {
+    res.json('Desde POST')
+})
+
+server.put('/', (req, res) => {
+    res.json('Desde PUT')
+})
+
+server.patch('/', (req, res) => {
+    res.json('Desde PATCH')
+})
+
+server.delete('/', (req, res) => {
+    res.json('Desde DELETE')
+})
+
+export default server
